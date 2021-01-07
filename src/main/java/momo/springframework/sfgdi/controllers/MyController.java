d4c1c852431427a571ea357fbb4f5b08ec617743
NeMoMo <piotr@theworlt.com>
2021-01-07 21:55:57 +0100

    32. The Spring Context

---
+++ src/main/java/momo/springframework/sfgdi/controllers/MyController.java
@@ -0,0 +1,13 @@
+package momo.springframework.sfgdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello() {
+        System.out.println("Hello world!!!!");
+
+        return "Hello everyone";
+    }
+}
