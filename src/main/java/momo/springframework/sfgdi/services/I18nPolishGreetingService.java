1eb7204e6fb1b422ed5834855d62d0c41d64c3a0
NeMoMo <piotr@theworlt.com>
2021-01-07 23:25:12 +0100

    39. Default Profile

--- src/main/java/momo/springframework/sfgdi/services/I18nPolishGreetingService.java
+++ src/main/java/momo/springframework/sfgdi/services/I18nPolishGreetingService.java
@@ -3,7 +3,7 @@ package momo.springframework.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("PL")
+@Profile({"PL", "default"})
 @Service("i18nService")
 public class I18nPolishGreetingService implements GreetingService {
 
